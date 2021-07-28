4845e06d85efe0f8912a3f1349aeb497aee2caee
Sander Revenberg <sander@revenberg.info>
2021-07-28 23:32:08 +0200

    .

--- files/watermeter.py
+++ files/watermeter.py
@@ -23,7 +23,7 @@ client.connect(mqttBroker, mqttPort, mqttKeepAlive)
 
 client.loop_start()
 
-client.subscribe("+/watermeter/#")
+client.subscribe("watermeter/#")
 client.on_message=on_message 
 
 # client.loop_forever()
