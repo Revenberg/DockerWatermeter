fd0fbf0cb8a70f06465cda33800c95a90ea3eef3
Sander Revenberg <sander@revenberg.info>
2021-07-29 16:29:21 +0200

    .

--- build.sh
+++ build.sh
@@ -8,4 +8,10 @@ docker image build -t revenberg/watermeter .
 # docker push revenberg/watermeter
 
 # testing: 
+
+echo "==========================================================="
+echo "=                                                         ="
+echo "=          docker run revenberg/watermeter                ="
+echo "=                                                         ="
+echo "==========================================================="
 docker run revenberg/watermeter
