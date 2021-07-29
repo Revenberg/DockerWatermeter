72a2c51c39e26b848f76aa4e539e01123862dc5e
Sander Revenberg <sander@revenberg.info>
2021-07-29 19:39:22 +0200

    .

--- files/watermeter.py
+++ files/watermeter.py
@@ -14,24 +14,19 @@ mqttPort = int(config.get('watermeter', 'mqttPort'))
 mqttKeepAlive = int(config.get('watermeter', 'mqttKeepAlive'))
 
 print(mqttBroker)
+
 current_value = 0
 pulse_count = 0
 
 def on_message(client, userdata, msg):
     global current_value 
     global pulse_count
-    print("test 1")
-    print(msg.topic + " " + str(msg.payload.decode("utf-8")))
     if msg.topic .lower() == "watermeter/reading/current_value" :
         current_value = int(str(msg.payload.decode("utf-8")))
     if msg.topic .lower() == "watermeter/reading/pulse_count" :
         pulse_count = int(str(msg.payload.decode("utf-8")))
-    print("test 2")
-    print( current_value )
-    print ( pulse_count )
-    
+
 def getData():
-    print("GetData")
     client = mqtt.Client("reader")
     client.connect(mqttBroker, mqttPort, mqttKeepAlive) 
 
