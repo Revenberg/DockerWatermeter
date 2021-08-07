ef288d42d30fa95b64c2c66514d53a3b670853d2
Sander Revenberg <sander@revenberg.info>
2021-08-07 17:35:39 +0200

    .

--- build.sh
+++ build.sh
@@ -4,9 +4,18 @@
 
 cd ~/dockerwatermeter
 
-rc=$(git remote show origin |  grep "local out of date" | wc -l)
+if [ -n "$1" ]; then
+  ex=$1
+else
+  rc=$(git remote show origin |  grep "local out of date" | wc -l)
+  if [ $rc -ne "0" ]; then
+    ex=true
+  else
+    ex=false
+  fi
+fi
 
-if [ $rc -ne "0" ]; then
+if [ $ex == true ]; then
     git pull
     chmod +x build.sh
 
